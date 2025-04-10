d6d8e4bd1b1d6f339703c9b36e9caa65b8a8b749
Cybernoob <ibrasamandar.2009@gmail.com>
2025-04-10 22:37:15 +0500

    added Swiper

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,32 @@
+import {Swiper, SwiperSlide} from "swiper/react"
+import {Navigation} from "swiper/modules"
+
 const Home = () => {
   return (
-    <div>Home</div>
+    <main className="main">
+      <Swiper
+        slidesPerView={1}
+        autoplay
+        navigation
+        modules={[Navigation]}
+      >
+        <SwiperSlide>
+          test 1
+        </SwiperSlide>
+        <SwiperSlide>
+          test 2
+        </SwiperSlide>
+        <SwiperSlide>
+          test 3
+        </SwiperSlide>
+        <SwiperSlide>
+          test 4
+        </SwiperSlide>
+        <SwiperSlide>
+          test 5
+        </SwiperSlide>
+      </Swiper>
+    </main>
   )
 }
 
