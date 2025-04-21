d923b26abc3aa2989ba82c832cc61327b9e5f062
Cybernoob <ibrasamandar.2009@gmail.com>
2025-04-21 21:41:47 +0500

    Swiper fixed

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,29 +1,86 @@
-import {Swiper, SwiperSlide} from "swiper/react";
-import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
 
-import 'swiper/scss';
-import 'swiper/scss/navigation';
-import 'swiper/scss/pagination';
-import 'swiper/scss/scrollbar';
+import 'swiper/css';
+import 'swiper/css/pagination';
+// import 'swiper/css/navigation';
 
 import './Home.scss';
+import Btn, { Sizes, Types } from "../../components/UI/Btn";
+import { Link } from "react-router";
 
 const Home = () => {
   return (
     <main className="main">
       <Swiper
-        spaceBetween={50}
+        spaceBetween={10}
         slidesPerView={1}
         onSlideChange={() => console.log('slide change')}
         onSwiper={(swiper) => console.log(swiper)}
-        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        modules={[Pagination]}
         className={"main__slider"}
+        cssMode
       >
-        <SwiperSlide><img src="https://swiperjs.com/images/sponsors/casinos-hunter.png" alt=""/></SwiperSlide>
-        <SwiperSlide><img src="https://swiperjs.com/images/sponsors/casinosfest.png" alt=""/></SwiperSlide>
-        <SwiperSlide><img src="https://swiperjs.com/images/sponsors/uusimmatkasinotcom.png" alt=""/></SwiperSlide>
-        <SwiperSlide><img src="https://swiperjs.com/images/sponsors/automatenspieler.png" alt=""/></SwiperSlide>
-      </Swiper>
+        <SwiperSlide>
+          <div className="main__slider-content">
+            <img src="/foods.png" alt=""/>
+            <div className="main__slider-content-info">
+              <p className="green">Welcome to shopery</p>
+              <h3>Fresh & Healthy<br/>Organic Food</h3>
+              <h4>Sale up to <span className="sale">30% OFF</span></h4>
+              <span className="small_grey">Free shipping on all your order. we deliver, you enjoy</span>
+              <Btn className="main__slider-content-info-btn" type={Types.button} size={Sizes.big}>
+                <span>Shop now <img src="/icons/whiteArrowRigth.svg" alt="" /></span>
+              </Btn>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="main__slider-content">
+            <img src="/foods.png" alt=""/>
+            <div className="main__slider-content-info">
+              <p className="green">Welcome to shopery</p>
+              <h3>Fresh & Healthy<br/>Organic Food</h3>
+              <h4>Sale up to <span className="sale">30% OFF</span></h4>
+              <span className="small_grey">Free shipping on all your order. we deliver, you enjoy</span>
+              <Link to="/shop">
+                <Btn className="main__slider-content-info-btn" type={Types.button} size={Sizes.big}>
+                  <span>Shop now <img src="/icons/whiteArrowRigth.svg" alt="" /></span>
+                </Btn>
+              </Link>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="main__slider-content">
+            <img src="/foods.png" alt=""/>
+            <div className="main__slider-content-info">
+              <p className="green">Welcome to shopery</p>
+              <h3>Fresh & Healthy<br/>Organic Food</h3>
+              <h4>Sale up to <span className="sale">30% OFF</span></h4>
+              <span className="small_grey">Free shipping on all your order. we deliver, you enjoy</span>
+              <Btn className="main__slider-content-info-btn" type={Types.button} size={Sizes.big}>
+                <span>Shop now <img src="/icons/whiteArrowRigth.svg" alt="" /></span>
+              </Btn>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="main__slider-content">
+            <img src="/foods.png" alt=""/>
+            <div className="main__slider-content-info">
+              <p className="green">Welcome to shopery</p>
+              <h3>Fresh & Healthy<br/>Organic Food</h3>
+              <h4>Sale up to <span className="sale">30% OFF</span></h4>
+              <span className="small_grey">Free shipping on all your order. we deliver, you enjoy</span>
+              <Btn className="main__slider-content-info-btn" type={Types.button} size={Sizes.big}>
+                <span>Shop now <img src="/icons/whiteArrowRigth.svg" alt="" /></span>
+              </Btn>
+            </div>
+          </div>
+        </SwiperSlide>
+        
+        </Swiper>
     </main>
   )
 }
