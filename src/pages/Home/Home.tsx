855bb2650b6acfdf8331390e396ffb4ec7fcf670
Cybernoob <ibrasamandar.2009@gmail.com>
2025-04-23 20:16:38 +0500

    Fix Swiper

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,13 @@
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination } from "swiper/modules";
+import {Swiper, SwiperSlide} from "swiper/react";
+import {Navigation, Pagination} from "swiper/modules";
 
 import 'swiper/css';
 import 'swiper/css/pagination';
 // import 'swiper/css/navigation';
 
 import './Home.scss';
-import Btn, { Sizes, Types } from "../../components/UI/Btn";
-import { Link } from "react-router";
+import Btn, {Sizes, Types} from "../../components/UI/Btn";
+import {Link} from "react-router";
 
 const Home = () => {
   return (
@@ -19,7 +19,6 @@ const Home = () => {
         onSwiper={(swiper) => console.log(swiper)}
         modules={[Pagination]}
         className={"main__slider"}
-        cssMode
       >
         <SwiperSlide>
           <div className="main__slider-content">
@@ -29,23 +28,9 @@ const Home = () => {
               <h3>Fresh & Healthy<br/>Organic Food</h3>
               <h4>Sale up to <span className="sale">30% OFF</span></h4>
               <span className="small_grey">Free shipping on all your order. we deliver, you enjoy</span>
-              <Btn className="main__slider-content-info-btn" type={Types.button} size={Sizes.big}>
-                <span>Shop now <img src="/icons/whiteArrowRigth.svg" alt="" /></span>
-              </Btn>
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="main__slider-content">
-            <img src="/foods.png" alt=""/>
-            <div className="main__slider-content-info">
-              <p className="green">Welcome to shopery</p>
-              <h3>Fresh & Healthy<br/>Organic Food</h3>
-              <h4>Sale up to <span className="sale">30% OFF</span></h4>
-              <span className="small_grey">Free shipping on all your order. we deliver, you enjoy</span>
-              <Link to="/shop">
+              <Link to={'/shop'}>
                 <Btn className="main__slider-content-info-btn" type={Types.button} size={Sizes.big}>
-                  <span>Shop now <img src="/icons/whiteArrowRigth.svg" alt="" /></span>
+                  <span>Shop now <img src="/icons/whiteArrowRigth.svg" alt=""/></span>
                 </Btn>
               </Link>
             </div>
@@ -59,9 +44,11 @@ const Home = () => {
               <h3>Fresh & Healthy<br/>Organic Food</h3>
               <h4>Sale up to <span className="sale">30% OFF</span></h4>
               <span className="small_grey">Free shipping on all your order. we deliver, you enjoy</span>
-              <Btn className="main__slider-content-info-btn" type={Types.button} size={Sizes.big}>
-                <span>Shop now <img src="/icons/whiteArrowRigth.svg" alt="" /></span>
-              </Btn>
+              <Link to={'/shop'}>
+                <Btn className="main__slider-content-info-btn" type={Types.button} size={Sizes.big}>
+                  <span>Shop now <img src="/icons/whiteArrowRigth.svg" alt=""/></span>
+                </Btn>
+              </Link>
             </div>
           </div>
         </SwiperSlide>
@@ -73,14 +60,31 @@ const Home = () => {
               <h3>Fresh & Healthy<br/>Organic Food</h3>
               <h4>Sale up to <span className="sale">30% OFF</span></h4>
               <span className="small_grey">Free shipping on all your order. we deliver, you enjoy</span>
-              <Btn className="main__slider-content-info-btn" type={Types.button} size={Sizes.big}>
-                <span>Shop now <img src="/icons/whiteArrowRigth.svg" alt="" /></span>
-              </Btn>
+              <Link to={'/shop'}>
+                <Btn className="main__slider-content-info-btn" type={Types.button} size={Sizes.big}>
+                  <span>Shop now <img src="/icons/whiteArrowRigth.svg" alt=""/></span>
+                </Btn>
+              </Link>
             </div>
           </div>
         </SwiperSlide>
-        
-        </Swiper>
+        <SwiperSlide>
+          <div className="main__slider-content">
+            <img src="/foods.png" alt=""/>
+            <div className="main__slider-content-info">
+              <p className="green">Welcome to shopery</p>
+              <h3>Fresh & Healthy<br/>Organic Food</h3>
+              <h4>Sale up to <span className="sale">30% OFF</span></h4>
+              <span className="small_grey">Free shipping on all your order. we deliver, you enjoy</span>
+              <Link to={'/shop'}>
+                <Btn className="main__slider-content-info-btn" type={Types.button} size={Sizes.big}>
+                  <span>Shop now <img src="/icons/whiteArrowRigth.svg" alt=""/></span>
+                </Btn>
+              </Link>
+            </div>
+          </div>
+        </SwiperSlide>
+      </Swiper>
     </main>
   )
 }
