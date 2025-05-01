47fa061e9e2af337367aa6bb064bc97df56c3138
Cybernoob <ibrasamandar.2009@gmail.com>
2025-05-01 21:56:01 +0500

    fixed grant info

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -15,8 +15,6 @@ const Home = () => {
       <Swiper
         spaceBetween={10}
         slidesPerView={1}
-        onSlideChange={() => console.log('slide change')}
-        onSwiper={(swiper) => console.log(swiper)}
         modules={[Pagination]}
         className={"main__slider"}
       >
@@ -87,27 +85,37 @@ const Home = () => {
       </Swiper>
       <div className="main__grant-info">
         <div className="main__grant-info-item">
-          <img src="/icons/deliveryTruck.svg" alt="delivery track icon" />
+          <img src="/icons/deliveryTruck.svg" alt="delivery track icon"/>
           <div className="main__grant-info-item-text">
             <h3>Free Shipping</h3>
             <p>Free shipping on all your order</p>
           </div>
         </div>
         <div className="main__grant-info-item">
-          <img src="/icons/headphones.svg" alt="headphone icon" />
+          <img src="/icons/headphones.svg" alt="headphone icon"/>
           <div className="main__grant-info-item-text">
             <h3>Customer Support 24/7</h3>
             <p>Instant access to Support</p>
           </div>
         </div>
         <div className="main__grant-info-item">
-          <img src="/icons/shoppingBag.svg" alt="bag icon" />
+          <img src="/icons/shoppingBag.svg" alt="bag icon"/>
           <div className="main__grant-info-item-text">
             <h3>100% Secure Payment</h3>
             <p>We ensure your money is save</p>
           </div>
         </div>
+        <div className="main__grant-info-item">
+          <img src="/icons/package.svg" alt="box icon"/>
+          <div className="main__grant-info-item-text">
+            <h3>Money-Back Guarantee</h3>
+            <p>30 Days Money-Back Guarantee</p>
+          </div>
+        </div>
       </div>
+      <section className="main__feat_products container">
+
+      </section>
     </main>
   )
 }
